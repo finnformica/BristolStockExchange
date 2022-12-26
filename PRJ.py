02acbe71f68d829a7831020d930404f0490f2272
finnformica <finnformica@gmail.com>
2022-12-26 23:23:12 +0000

    implement mutation function

--- PRJ.py
+++ PRJ.py
@@ -16,56 +16,81 @@ class Trader_PRJ(Trader_PRZI):
         self.F = params['F'] # differential evolution weight
         self.k = params['k'] # number of strategies
         self.c = params['c'] # 
-        self.p = params['p']
+        self.p = params['p'] # top 100p% for best strat
         self.muF = 0.5 # determines cauchy distribution for F
         
 
-    def mutatation(self):
+    def mutation(self):
         '''
         Generate a new mutant strategy based on the JADE mutation formula
         '''
-        
-        # pick four distinct strats at random
+        # order strats by pps descending
+        ordered = sorted(self.strats, key=lambda x: self.strats[x]['pps'], reverse=True)
+        quantile = ordered[: int(self.p * self.k)] # select top p-quantile
+        random.shuffle(quantile)
+        s_best = quantile[0] # top 100p% chosen at random
+
+        # pick k distinct strategies at random
         stratlist = list(range(0, self.k))
         random.shuffle(stratlist)
 
         # s0 is next iteration's candidate for possible replacement
         self.diffevol['s0_index'] = stratlist[0]
 
-        # s1, s2, s3 used in DE to create new strategy, potential replacement for s0
-        i1, i2, i3 = stratlist[1], stratlist[2], stratlist[3]
-        s1, s2, s3 = self.strats[i1]['stratval'], self.strats[i2]['stratval'], self.strats[i3]['stratval']
+        s0 = self.strats[self.diffevol['s0_index']]['stratval']
+        s1 = stratlist[1] # randomly choose from current population
 
-        s_best = 0
+        intersection = [self.strats[i]['stratval'] for i in stratlist[2:]] + self.archive
+        random.shuffle(intersection)
+        s2 = intersection[0] # randomly choose from current population intersect archive
 
-        # this is the differential evolution "adaptive step": create a new individual
-        new_stratval = s1 + self.F * (s_best - s1) + self.F * (s2 - s3)
-
-        # clip to bounds
-        new_stratval = max(-1, min(+1, new_stratval))
+        # differential evolution mutation
+        new_stratval = s0 + self.F * (s_best - s0) + self.F * (s1 - s2)
+        new_stratval = max(-1, min(1, new_stratval)) # clip to bounds
 
         # record it for future use (s0 will be evaluated first, then s_new)
         self.strats[self.diffevol['snew_index']]['stratval'] = new_stratval
 
 
-    def crossover(self):
-        pass
-
     def selection(self):
-        pass
+        '''
+        Select the most profitable strat between s0 and s_new
+        '''
+
+        i_0 = self.diffevol['s0_index']
+        i_new = self.diffevol['snew_index']
+
+        if self.strats[i_new]['pps'] >= self.strats[i_0]['pps']:
+            # archive unsuccessful strat
+            self.archive.append(self.strats[i_0]['stratval'])
+
+            # overwrite s0
+            self.strats[i_0]['stratval'] = self.strats[i_new]['stratval']
+
+            # store successful F value
+            self.display.append(self.F)
 
     def clean_archive(self):
-        if self.archive > self.k:
-            rand_strat = random.randint(0, self.k - 1)
-            self.archive.pop(rand_strat)
+        '''
+        Remove strats from archive if length exceeds k
+        '''
+        if len(self.archive) > self.k:
+            random.shuffle(self.archive)
+            self.archive = self.archive[:self.k]
     
     def lehmer_mean(self):
+        '''
+        Calculate the Lehmer mean for updating muF
+        '''
         sum_sqr = sum([f ** 2 for f in self.display])
         sum = sum(self.display)
 
-        return sum_sqr / sum
+        return sum_sqr / (sum if sum else 0.0001)
 
     def generate_F(self):
+        '''
+        Generate a new DE weight using Cauchy distribution
+        '''
         while True:
             val = cauchy.rvs(self.muF, 0.1)
             if val > 0:
@@ -74,8 +99,30 @@ class Trader_PRJ(Trader_PRZI):
         self.F = min(val, 1)
         
 
+    def check_strat_convergence(self):
+        # is the stddev of the strategies in the population equal/close to zero?
+        sum = 0.0
+        for s in range(self.k):
+            sum += self.strats[s]['stratval']
+        strat_mean = sum / self.k
+        sumsq = 0.0
+        for s in range(self.k):
+            diff = self.strats[s]['stratval'] - strat_mean
+            sumsq += (diff * diff)
+        strat_stdev = math.sqrt(sumsq / self.k)
+        
+        if strat_stdev < 0.0001:
+            # this population has converged
+            # mutate one strategy at random
+            randindex = random.randint(0, self.k - 1)
+            self.strats[randindex]['stratval'] = random.uniform(-1.0, +1.0)
+
     def update_params(self):
-        self.clean_archive()
+        self.clean_archive() # ensure archive len < k
+        self.display = []
+
+        # update cauchy mean
+        self.muF = (1 - self.c) * self.muF + self.c * self.lehmer_mean()
 
 
     def respond(self, time, lob, trade, verbose):
@@ -85,68 +132,27 @@ class Trader_PRJ(Trader_PRZI):
         if self.k < 4:
             sys.exit('FAIL: k too small for diffevol')
 
-        self.update_params() # ensure archive len < k
-
         # only initiate diff-evol once the active strat has been evaluated for long enough
         actv_lifetime = time - self.strats[self.active_strat]['start_t']
         if actv_lifetime >= self.strat_wait_time:
-
+            
+            # s0 evaluated, next evaluate s_new
             if self.diffevol['de_state'] == 'active_s0':
-                # s0 evaluated, next evaluate s_new
+                
                 self.active_strat = self.diffevol['snew_index']
                 self.strats[self.active_strat]['start_t'] = time
                 self.strats[self.active_strat]['profit'] = 0.0
                 self.strats[self.active_strat]['pps'] = 0.0
                 self.diffevol['de_state'] = 'active_snew'
 
+            # evaluated both s0 and s_new, complete DE algorithm
             elif self.diffevol['de_state'] == 'active_snew':
-                # evaluated both s0 and s_new, complete DE
-                i_0 = self.diffevol['s0_index']
-                i_new = self.diffevol['snew_index']
-
-                # check for highest pps between s0 and s_new
-                if self.strats[i_new]['pps'] >= self.strats[i_0]['pps']:
-                    # archive unsuccessful strat
-                    self.archive.append(self.strats[i_0]['stratval'])
-
-                    # overwrite s0
-                    self.strats[i_0]['stratval'] = self.strats[i_new]['stratval']
-
-                    # store successful F value
-                    self.display.append(self.F)
-
-                # ADAPTIVE DIFFERENTIAL EVOLUTION
-
-                self.generate_F()
-
-                self.mutation()
-
-
-
-
-
-
-
-
-
-
-                # DC's intervention for fully converged populations
-                # is the stddev of the strategies in the population equal/close to zero?
-                sum = 0.0
-                for s in range(self.k):
-                    sum += self.strats[s]['stratval']
-                strat_mean = sum / self.k
-                sumsq = 0.0
-                for s in range(self.k):
-                    diff = self.strats[s]['stratval'] - strat_mean
-                    sumsq += (diff * diff)
-                strat_stdev = math.sqrt(sumsq / self.k)
                 
-                if strat_stdev < 0.0001:
-                    # this population has converged
-                    # mutate one strategy at random
-                    randindex = random.randint(0, self.k - 1)
-                    self.strats[randindex]['stratval'] = random.uniform(-1.0, +1.0)
+                self.generate_F()
+                
+                # ADAPTIVE DIFFERENTIAL EVOLUTION (JADE)
+                self.selection()
+                self.mutation()
 
                 # set up next iteration: first evaluate s0
                 self.active_strat = self.diffevol['s0_index']
@@ -155,6 +161,9 @@ class Trader_PRJ(Trader_PRZI):
                 self.strats[self.active_strat]['pps'] = 0.0
 
                 self.diffevol['de_state'] = 'active_s0'
+
+                # reset for next generation
+                self.update_params()
 
             else:
                 sys.exit('FAIL: self.diffevol[\'de_state\'] not recognized')
